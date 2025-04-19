8058d64de4a41f25899cfab50bb2eb3a21c44da0
shhhyung <shahadalenzi604@gmail.com>
2025-04-19 21:19:06 +0300

    Update Album.java

--- LinkedList/Album.java
+++ LinkedList/Album.java
@@ -1,3 +1,105 @@
 public class Album {
-    
+    private String name;
+    private String condition;
+    private PhotoManager manager;
+    private int NbComps;
+
+    // Constructor
+    public Album(String name, String condition, PhotoManager manager) {
+        this.name = name;
+        this.condition = condition;
+        this.manager = manager;
+        NbComps = 0;
+    }
+
+    // Return the name of the album
+    public String getName() {
+        return name;
+    }
+
+    // Return the condition associated with the album
+    public String getCondition() {
+        return condition;
+    }
+
+    // Return the manager
+    public PhotoManager getManager() {
+        return manager;
+    }
+
+    // Return all photos that satisfy the album condition
+    public LinkedList<Photo> getPhotos() {
+        LinkedList<Photo> photos1 = manager.getPhotos();
+        LinkedList<Photo> photos2 = new LinkedList<Photo>();
+
+        if (!photos1.empty()) {
+            photos1.findFirst();
+            while (!photos1.last()) {
+                photos2.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+                photos1.findNext();
+            }
+            photos2.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+        }
+
+        NbComps = 0;
+        if (this.condition.compareTo("") != 0) {
+            String[] Array = condition.split("AND");
+            photos2.findFirst();
+            while (!photos2.last()) {
+                Photo photo = photos2.retrieve();
+                if (!allAvailable(photo.getTags(), Array)) {
+                    photos2.remove();
+                } else {
+                    photos2.findNext();
+                }
+            }
+
+            // Handle last photo separately
+            Photo lastPhoto = photos2.retrieve();
+            if (!allAvailable(lastPhoto.getTags(), Array)) {
+                photos2.remove();
+            } else {
+                photos2.findNext();
+            }
+        }
+        return photos2;
+    }
+
+    // Return the number of tag comparisons used to find all photos of the album
+    public int getNbComps() {
+        return NbComps;
+    }
+
+    private boolean allAvailable(LinkedList<String> AllTags, String[] Arr) {
+        boolean continue1 = true;
+        if (AllTags.empty()) {
+            continue1 = false;
+        } else {
+            for (int i = 0; i < Arr.length && continue1; i++) {
+                boolean tagMatched = false;
+                AllTags.findFirst();
+                while (!AllTags.last()) {
+                    this.NbComps++;
+                    if (AllTags.retrieve().compareToIgnoreCase(Arr[i]) == 0) {
+                        tagMatched = true;
+                        break;
+                    }
+                    AllTags.findNext();
+                }
+
+                // Check the last tag
+                if (!tagMatched) {
+                    this.NbComps++;
+                    if (AllTags.retrieve().compareToIgnoreCase(Arr[i]) == 0) {
+                        tagMatched = true;
+                    }
+                }
+
+                if (!tagMatched) {
+                    continue1 = false;
+                }
+            }
+        }
+        return continue1;
+    }
 }
