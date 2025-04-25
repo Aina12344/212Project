9581249ef1092814d4167e383b868a1dc65cb1ab
amjad7m <amjadmm711@gmail.com>
2025-04-26 00:35:17 +0300

    Update Photo.java

--- LinkedList/Photo.java
+++ LinkedList/Photo.java
@@ -1,43 +1,37 @@
 public class Photo {
-   private String path;
-   LinkedList<String> photoTags = new LinkedList<>();
-   public Photo(String path, LinkedList<String> tags){
-      this.path = path;     
-      if (! tags.empty()) {
-         tags.findFirst();
-         do {                     
-            photoTags.insert(tags.retrieve());
-            if (tags.last())
-               break;
-            tags.findNext();
-         } while (true);
-      }
-   }        
-   public String getPath(){
-      return path;
-   }       
-   public LinkedList<String> getTags(){
-      LinkedList<String> tagsCopy = new LinkedList<String>();
-            
-      if (! photoTags.empty()){          
-         photoTags.findFirst();
-         do {
-            tagsCopy.insert(photoTags.retrieve());
-            if(photoTags.last())
-               break;
-            photoTags.findNext();
-         } while (true); 
-      }
-      return tagsCopy;
-   }
+ private String path;
+ LinkedList<String> photoTags = new LinkedList<>();
+ //costructor
+    public Photo(String path, LinkedList<String> tags){
+ this.path = path;     
+ if (! tags.empty()) {
+  tags.findFirst();
+do {                     
+photoTags.insert(tags.retrieve());
+if (tags.last())
+break;
+tags.findNext();
+} while (true);
+ }
+} 
+ // Return the full file name (the path) of the photo. A photo is uniquely identified by its path.       
+ public String getPath(){
+ return path;
+ }  
+  // Return all tags associated with the photo     
+public LinkedList<String> getTags(){
 
-   public String toString() {
-      String str = "Photo{" + "path=" + path + ", photoTags=" ;
-      photoTags.findFirst();
-      while ( ! photoTags.last()) {
-         str += photoTags.retrieve().toString() + "; ";
-         photoTags.findNext();
-      }    
-      str += photoTags.retrieve().toString()  + "}";
-      return str ;
-   } }
+            
+return photoTags;
+}
+
+public String toString() {
+String str = "Photo{" + "path=" + path + ", photoTags=" ;
+photoTags.findFirst();
+while ( ! photoTags.last()) {
+str += photoTags.retrieve().toString() + "; ";
+photoTags.findNext();
+}    
+str += photoTags.retrieve().toString()  + "}";
+return str ;
+} }
